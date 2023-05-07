254cb6e34e93c874ee989d356154e55abfb33b3b
M-Zubair10 <47709300+M-Zubair10@users.noreply.github.com>
2023-05-07 13:16:07 +0500

    Delete CaptchaResolver/solutions/recaptcha directory

--- CaptchaResolver/solutions/recaptcha/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from .detector import Model as Detector
-
-detector = Detector()
-
-from .label import clean_label, merge_classes, solved_objects, grounded_objects
-
-
-__all__ = ['detector', 'clean_label', 'merge_classes', 'solved_objects', 'grounded_objects']
